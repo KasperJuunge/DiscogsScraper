a08ceb800c5e065d7b8dd20a57b9a867ab2a99e9
Kasper Juunge <39537120+KasperJuunge@users.noreply.github.com>
2018-09-27 08:19:53 +0200

    Files added.

---
+++ Discogs_Scraper/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace Discogs_Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "https://www.discogs.com";
+
+
+        }
+    }
+
+    public class DiscogsScraper
+    {
+        private string startingPage; //Start page to begin scraping
+        private int numberOfReleases;
+
+        private HtmlDocument GetStartingPage(string url)
+        {
+
+            //return htmlDoc;
+        }
+        private string GetCoverImgUrl(HtmlNode htmlNode)
+        {
+            return imgUrl;
+        }
+        private string GetGenre(HtmlNode htmlNode)
+        {
+            return genre;
+        }
+        private string GetTitle(HtmlNode htmlNode)
+        {
+            return title;
+        }
+        private string GetReleaseYear(HtmlNode htmlNode)
+        {
+            return releaseYear;
+        }
+
+
+    }
+
+    public class DataBaseHandler
+    {
+
+           
+
+    }
+    
+    // Functions:
+    //      GetStartingPage(string url);
+    //      ScrapePages(int numberOfReleases, bool pureGenres);
+    //
+    //      GetGenre(HtmlDocument hmtlDoc);
+    //      GetTitle(HtmlDocument hmtlDoc);
+    //      GetReleaseYear(HtmlDocument hmtlDoc);
+    //      
+
+
+
+}
